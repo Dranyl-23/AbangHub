da784f0b2eba010b27c7d7db7027b968a18ab2d1
Dranyl <Alfielynard23@gmail.com>
2026-07-25 02:04:37 +0800

    chore: enable raw php errors

--- rentease/server.php
+++ rentease/server.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Vercel serverless environment overrides for read-only filesystem
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
